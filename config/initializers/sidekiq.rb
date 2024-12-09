0d548d4bb1dea7728a2ed74382fef5401654a5bb
Eddy <155239294+Eddy3129@users.noreply.github.com>
2024-12-09 21:35:24 +0800

    removed sidekiq namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@
 # config/initializers/sidekiq.rbRAILS_ENV=production bundle exec rails server
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), namespace: 'myapp_production' }
+  config.redis = { url: ENV.fetch('REDIS_URL') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), namespace: 'myapp_production' }
+  config.redis = { url: ENV.fetch('REDIS_URL') }
 end
